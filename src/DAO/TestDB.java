ff271ef480478cfed94263de1b57985e08324474
5H <5H@INFO1-PC01>
2019-01-09 10:40:03 +0100

    Prima versione

---
+++ src/DAO/TestDB.java
@@ -0,0 +1,16 @@
+package DAO;
+
+public class TestDB {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 	This is a main to check the DB connection
+		 */
+		
+		CorsoDAO cdao = new CorsoDAO();
+		cdao.getTuttiICorsi();
+
+	}
+
+}
---
+++ src/model/TestModel.java
@@ -0,0 +1,15 @@
+package model;
+
+public class TestModel {
+
+	public static void main(String[] args) {
+		
+		Model model = new Model();
+		
+		/*
+		 * 	Write here your test model
+		 */
+
+	}
+
+}
